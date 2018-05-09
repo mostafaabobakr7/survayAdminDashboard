0e26c260474c24f8f0bb60ff5e58360eabb52385
mostafaabobakr7 <mostafaabobakr777@gmail.com>
2018-05-09 16:36:07 +0200

    FINISHED: modal create new project

--- src/js/script.js
+++ src/js/script.js
@@ -17,7 +17,6 @@ $('#folder__menu-close').on("click", function () {
 /* section-home-folders-dropdown */
 $('#folderDropdown').on('click', function () {
   $(this).toggleClass('dropDownClickBorder')
-  console.log(this)
 })
 $('#folderDropdown-menu .dropdown-item').map(function () {
   $(this).on('click', function () {
@@ -25,3 +24,17 @@ $('#folderDropdown-menu .dropdown-item').map(function () {
   })
 })
 /* section-home-folders-dropdown end------ */
+
+/* create Blank Project */
+$('#BlankSurvayProject').on('click', function () {
+  $(this).addClass('d-none');
+  $('#CreateFromExisting').addClass('d-none');
+  $('.list-Research').removeClass('d-none')
+})
+/* create Blank Project end------------ */
+/* create Project Back */
+$('#createProjectBack').on('click', function () {
+  $(this).parent().addClass('d-none')
+  $('#CreateFromExisting, #BlankSurvayProject').removeClass('d-none');
+})
+/* create Project Back end-------------*/
