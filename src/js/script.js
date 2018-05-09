1b8ea8c07ca35b8e021da517423a5e8e6ccc5b85
mostafaabobakr7 <mostafaabobakr777@gmail.com>
2018-05-09 21:14:12 +0200

    ADD: card-list in home page

--- src/js/script.js
+++ src/js/script.js
@@ -5,10 +5,11 @@ $('#folder__link').on("click", function () {
   $(this)
     .parent()
     .toggleClass('menu__folder')
+  $('.projects-list-container').toggleClass('menu__folder')
   $('#folder__menu').toggleClass('menu__close')
 })
 $('#folder__menu-close').on("click", function () {
-  $('#folder__link')
+  $('#folder__link, .projects-list-container')
     .parent()
     .toggleClass('menu__folder')
   $('#folder__menu').toggleClass('menu__close')
@@ -41,7 +42,48 @@ $('#createProjectBack').on('click', function () {
 /* create project */
 $('#createProjectBTN').on('click', function () {
   const projectName = $('#projectName').val();
-  console.log(projectName)
+  const d = new Date();
+const strDate = d.getFullYear() + "/" + (d.getMonth()+1) + "/" + d.getDate();
+  var card = $(`<div class="row py-3">
+  <div class="col-md-12">
+    <div class="projects-list-container pl-5">
+      <div class="card">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-md-3">
+              <h6 class="card-title">
+                <img width="24" height="24" src="./img/research_core.svg" alt="research core img">
+                <span class="pl-1"> Survey</span>
+              </h6>
+              <h2 class="projectName">${projectName}</h2>
+              <p class="card-text">Modified ${strDate}</p>
+            </div>
+            <div class="col-md-9 d-flex justify-content-end">
+            <div class="card-body__details d-flex justify-content-start">
+              <div class="card-body__details-status ">
+                <h2>NEW</h2>
+                <h6>Statues</h6>
+              </div>
+              <div class="card-body__details-questions ">
+                <h2>1</h2>
+                <h6>Questions</h6>
+              </div>
+              <div class="card-body__details-languages ">
+                <h2>0</h2>
+                <h6>Languages</h6>
+              </div>
+            </div>
+            <span class="card-body__details-icon">
+              ...
+            </span>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`);
+  $('.projects-list .container-fluid').append(card)
 })
 // when press Enter on keyboard as create project
 $('#projectName').keyup(function (event) {
