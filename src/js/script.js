7298e607e641102c5766e8d8a2b08be2bc536936
mostafaabobakr7 <mostafaabobakr777@gmail.com>
2018-05-10 10:45:15 +0200

    FINISH: home page create new project

--- src/js/script.js
+++ src/js/script.js
@@ -44,46 +44,82 @@ $('#createProjectBack').on('click', function () {
 $('#createProjectBTN').on('click', function () {
   const projectCreationName = $('#projectName').val();
   const d = new Date();
-  const projectCreationDate= d.toLocaleString('en-US', { day:'numeric', month:'long',year:'numeric', hour:'numeric', minute: 'numeric', hour12: true });
-  var card = $(`<div class="row py-3">
-  <div class="col-md-12">
-    <div class="projects-list-container pl-5">
-      <div class="card">
-        <div class="card-body">
-          <div class="row">
-            <div class="col-md-3">
-              <h6 class="card-title">
-                <img width="24" height="24" src="./img/research_core.svg" alt="research core img">
-                <span class="pl-1"> Survey</span>
-              </h6>
-              <h2 class="projectName">${projectCreationName}</h2>
-              <p class="card-text">Modified at: ${projectCreationDate}</p>
-            </div>
-            <div class="col-md-9 d-flex justify-content-end">
-              <div class="card-body__details d-flex justify-content-start">
-                <div class="card-body__details-status ">
-                  <h2>NEW</h2>
-                  <h6>Statues</h6>
-                </div>
-                <div class="card-body__details-questions ">
-                  <h2>1</h2>
-                  <h6>Questions</h6>
-                </div>
-                <div class="card-body__details-languages ">
-                  <h2>0</h2>
-                  <h6>Languages</h6>
+  const projectCreationDate = d.toLocaleString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+  });
+  var card = $(`        <div class="row py-3">
+          <div class="col-md-12">
+            <div class="projects-list-container pl-5">
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-md-3">
+                      <h6 class="card-title">
+                        <img width="24" height="24" src="./img/research_core.svg" alt="research core img">
+                        <span class="pl-1"> Survey</span>
+                      </h6>
+                      <h2 class="projectName">${projectCreationName}</h2>
+                      <p class="card-text">Modified at: ${projectCreationDate}</p>
+                    </div>
+                    <div class="col-md-9 d-flex justify-content-end">
+                      <div class="card-body__details d-flex justify-content-start">
+                        <div class="card-body__details-status ">
+                          <h2>NEW</h2>
+                          <h6>Statues</h6>
+                        </div>
+                        <div class="card-body__details-questions ">
+                          <h2>1</h2>
+                          <h6>Questions</h6>
+                        </div>
+                        <div class="card-body__details-languages ">
+                          <h2>0</h2>
+                          <h6>Languages</h6>
+                        </div>
+                      </div>
+                      <div class="card-body-icon " id="projectCardDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+                        <div class="dropdown-menu" aria-labelledby="projectCardDropdown">
+                          <a class="dropdown-item">
+                            <i class="fa fa-check-filled"></i>Activate</a>
+                          <li class="dropdown-divider"></li>
+                          <a class="dropdown-item">
+                            <i class="fa fa-collaborate-lg"></i>Collaborate</a>
+                          <a class="dropdown-item">
+                            <i class="fa fa-folder"></i>Reveal in Folder</a>
+                          <a class="dropdown-item">
+                            <i class="fa fa-text"></i>Rename Project</a>
+                          <a class="dropdown-item">
+                            <i class="fa fa-squarestack"></i>Copy Project</a>
+                          <li class="dropdown-divider"></li>
+                          <a class="dropdown-item">
+                            <i class="fa fa-pencil"></i>Edit Survey</a>
+                          <a class="dropdown-item">
+                            <i class="fa fa-open"></i>Preview Survey</a>
+                          <a class="dropdown-item">
+                            <i class="fa fa-translate"></i>Translate Survey</a>
+                          <a class="dropdown-item">
+                            <i class="fa fa-airplane-wire"></i>Distribute Survey</a>
+                          <a class="dropdown-item">
+                            <i class="fa fa-comment"></i>Data &amp; Analysis</a>
+                          <a class="dropdown-item">
+                            <i class="fa nav-fa-bargraph-unboxed-alt"></i>View Reports</a>
+                          <li class="dropdown-divider"></li>
+                          <a class="dropdown-item">
+                            <i class="fa fa-x-filled"></i>Delete Project</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <span class="card-body-icon">
-                <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
-              </span>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>`);
+        </div>`);
   $('.projects-list .container-fluid').append(card)
 })
 // when press Enter on keyboard as create project
